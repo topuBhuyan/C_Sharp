27d49899022ae8f6b1cdebc83134e78bdf087518
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-06-04 16:45:39 +0600

    Add files via upload
    Signed-off-by: Topu Bhuyan <113087685+xvy45@users.noreply.github.com>

---
+++ Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Emplyee em = new Emplyee("moja", "rahim");
+            em.printFullName();
+        }
+    }
+}
+class Emplyee
+{
+    string _firstName;
+    string _lastName;
+
+    public Emplyee (string firstName, string lastname)
+    {
+        _firstName = firstName;
+        _lastName = lastname;
+    }
+    public void printFullName()
+    {
+        Console.WriteLine("FullNmae : {0} {1}", _firstName, _lastName);
+    }
+}
