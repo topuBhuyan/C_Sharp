ecec2b023bbe06186189af497b6fd448e9cf7dc6
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-06-04 16:44:26 +0600

    Add files via upload
    Signed-off-by: Topu Bhuyan <113087685+xvy45@users.noreply.github.com>

---
+++ Namespace/Program.cs
@@ -0,0 +1,50 @@
+﻿using pata = ProjectA.TeamA;
+using pata = ProjectA.TeamB;
+
+using System;
+using System.Runtime.InteropServices;
+namespace Namespace
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            pata.ClassA class = new pata.ClassA();
+            ClassA.printhello();
+
+            pata.ClassB class = new pata.ClassA();
+            ClassB.printhello();
+        }
+    }
+}
+
+
+//namespace projectA
+//{
+//    namespace TeamA
+//    {
+//        class ClassA
+//        {
+//            public static void printhello()
+//            {
+//                Console.Write("Hello....");
+//            }
+//        }
+//    }
+//}
+////////////// using dot. //////////
+/////
+//namespace projectA.TeamA
+//{
+//        class ClassA
+//        {
+//           public static void printhello()
+//            {
+//                Console.Write("Hello....");
+//            }
+//        }
+//}
+
+
+
+
