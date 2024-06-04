c360289875fc0b5c7923156459ac805caf868645
Topu Bhuyan <113087685+xvy45@users.noreply.github.com>
2024-06-04 16:48:12 +0600

    Add files via upload
    Signed-off-by: Topu Bhuyan <113087685+xvy45@users.noreply.github.com>

---
+++ Properties/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            {
+                s.name = null;
+                s.id = -10;
+                s.rol = 010;
+                s.passMark = 10;
+
+                Console.WriteLine("id={0}, name={1}, rol ={2}, passMark{3}",s.name, s.id, s.rol, s.passMark);
+            }
+
+        }
+
+        //class Student
+        //{
+        //    public string name;
+        //    public int id;
+        //    public int rol;
+        //    public int passMark;
+        //}
+
+
+    }
+}
