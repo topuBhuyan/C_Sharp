5c50364653be78e1a94c0c2f09ec565f96d809ef
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-11-18 19:06:05 +0600

    Add files via upload
    Signed-off-by: Topu  <113087685+topuBhuyan@users.noreply.github.com>

---
+++ User Input/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.Write("Enter your Name: ");
+string? name = Console.ReadLine();
+Console.WriteLine("student is : " + name);
+
+Console.Write("Enter your Age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Age: " + age);
+
+Console.Write("Have you already register ! ");
+bool is_register = Convert.ToBoolean(Console.ReadLine());
+Console.WriteLine("Is compleate register: " + is_register);
