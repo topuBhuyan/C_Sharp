83f66cdf037a1ad32ae8da7b70e054773df83460
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-11-18 19:02:52 +0600

    Add files via upload
    Signed-off-by: Topu  <113087685+topuBhuyan@users.noreply.github.com>

---
+++ 4_ Basic Structure/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+using System;
+class clas_name{
+    public static void Main(string[] args){
+        //System.Console.WriteLine(123);  // writeLine is Method, system is class, console is member
+        Console.WriteLine(123);
+        Console.WriteLine("Topu Bhuyan");
+    }
+}
+*/
+//5_printing bio
+
+using System;
+
+class class_name{
+    public static void Main(string[] args){
+        Console.Write("My name is Topu . "); // write 1 line a print kore 
+        Console.WriteLine("I'm 21 yers old"); // writeLine new Line print kore
+        Console.WriteLine("CSE Student");
+    }
+}
