f1df9ae2340ac49550436d4db23d4090d4687c8f
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-11-18 19:05:02 +0600

    Add files via upload
    Signed-off-by: Topu  <113087685+topuBhuyan@users.noreply.github.com>

---
+++ Type Casting/Program.cs
@@ -0,0 +1,10 @@
+﻿double number = 100.50;
+Console.WriteLine(number);
+
+int number1 = (int)number;
+Console.WriteLine(number1);
+
+int number2 = 3050;
+Console.WriteLine(Convert.ToString(number2));
+Console.WriteLine(Convert.ToChar(number2));
+
