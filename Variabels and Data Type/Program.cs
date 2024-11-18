b8330963ed8d777cf307413ad2b4912462dee11f
Topu <113087685+topuBhuyan@users.noreply.github.com>
2024-11-18 19:07:03 +0600

    Add files via upload
    Signed-off-by: Topu  <113087685+topuBhuyan@users.noreply.github.com>

---
+++ Variabels and Data Type/Program.cs
@@ -0,0 +1,14 @@
+﻿string name;
+name = "topu";
+Console.WriteLine(name);
+Console.WriteLine(name);
+name = "bhuyan";
+Console.Write("Bhuyan\n");
+Console.Write("Bhuyan\n");
+
+int Date_of_birth;
+Date_of_birth = 2003;
+Console.WriteLine(Date_of_birth);
+
+bool true_and_fals = true;
+Console.Write(true_and_fals);
